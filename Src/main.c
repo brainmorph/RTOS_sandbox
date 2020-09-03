47dec727a0c7001e300f61c686c20438e55b4007
brainmorph <capota.d@gmail.com>
2020-09-02 22:41:55 -0500

    Enable I2C3 and add logging source/header

--- Src/main.c
+++ Src/main.c
@@ -21,6 +21,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -91,6 +92,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C3_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
 
